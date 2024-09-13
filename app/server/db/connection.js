b066af4f8e5cfbbb2fc6fcd93c78ad48d5cf1d30
rsmith-github <rsmith.git@gmail.com>
2024-09-13 08:23:18 +0100

    Revert connection to previous version

--- app/server/db/connection.js
+++ app/server/db/connection.js
@@ -1,7 +1,6 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
 const URI = process.env.ATLAS_URI || "";
-
 const client = new MongoClient(URI, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -10,26 +9,16 @@ const client = new MongoClient(URI, {
   },
 });
 
-let db; // Declare a variable to hold the database connection
-
-async function connectDB() {
-  if (!db) {
-    try {
-      // Connect the client to the server
-      await client.connect();
-      // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-
-      // Set the db variable to your database name
-      db = client.db("employees"); // Replace with your actual database name
-    } catch (err) {
-      console.error("Failed to connect to MongoDB:", err);
-      throw err; // Re-throw the error to be handled by the caller
-    }
-  }
-
-  return db; // Return the connected database instance
+try {
+  // Connect the client to the server
+  await client.connect();
+  // Send a ping to confirm a successful connection
+  await client.db("admin").command({ ping: 1 });
+  console.log("Pinged your deployment. You successfully connected to MongoDB!");
+} catch (err) {
+  console.log("🚀 ~ err:", err)
 }
 
-export default connectDB;
+let db = client.db("employees");
+
+export default db;
